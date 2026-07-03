9dfab2a7c559f6c89bfa32dcb136b739e6e2b815
wendypatiencemu-pixel <wendypatiencemu@gmail.com>
2026-07-03 05:41:07 -0700

    Add files via upload

---
+++ GradeChecker.py
@@ -0,0 +1,8 @@
+Email = "wendypatiencemu@gmail.com"
+Password = "wendypatie"
+email = input("Enter your email: ")
+password = input("Enter your password: ")
+if email == Email and password == Password :
+    print("login successful")
+else:
+    print("login failed")
